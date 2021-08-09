daee943e00977be6fd096e56d7b7c11412191aec
hxj8059 <32088255+hxj8059@users.noreply.github.com>
2021-08-09 14:11:29 +0800

    resources

---
+++ resources/DBServer.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class DBServer
+{
+    public DBServer(int portNumber)
+    {
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            System.out.println("Server Listening");
+            while(true) processNextConnection(serverSocket);
+        } catch(IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+
+    private void processNextConnection(ServerSocket serverSocket)
+    {
+        try {
+            Socket socket = serverSocket.accept();
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            System.out.println("Connection Established");
+            while(true) processNextCommand(socketReader, socketWriter);
+        } catch(IOException ioe) {
+            System.err.println(ioe);
+        } catch(NullPointerException npe) {
+            System.out.println("Connection Lost");
+        }
+    }
+
+    private void processNextCommand(BufferedReader socketReader, BufferedWriter socketWriter) throws IOException, NullPointerException
+    {
+        String incomingCommand = socketReader.readLine();
+        System.out.println("Received message: " + incomingCommand);
+        socketWriter.write("[OK] Thanks for your message: " + incomingCommand);
+        socketWriter.write("\n" + ((char)4) + "\n");
+        socketWriter.flush();
+    }
+
+    public static void main(String args[])
+    {
+        DBServer server = new DBServer(8888);
+    }
+
+}
